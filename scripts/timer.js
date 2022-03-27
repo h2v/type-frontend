0637efa3cbd8cace8391b7d1a4bfd394cd84126e
VIIICMXCI <98252522+VIIICMXCI@users.noreply.github.com>
2022-03-26 20:09:39 -0400

    im gonna cry
    why no align men((

---
+++ scripts/timer.js
@@ -0,0 +1,13 @@
+const lenOpts = {
+	qtrMin : 15,
+	hlfMin : 30,
+	min : 60,
+	minHlf : 120,
+}
+
+
+var countdown;
+    setInterval(function() {
+        if (document.getElementById())
+        console.log(lenOpts["min"]);;
+}, 1000);
